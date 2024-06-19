1ce6bf454e0319267a4e07703d6d2b0468594db9
Dieoscar <dkloster79@yahoo.com.ar>
2024-06-18 21:57:33 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        dtvVectorPrincipal = New DataGridView()
+        dtvNuevoVector = New DataGridView()
+        cmdEjecutar = New Button()
+        CType(dtvVectorPrincipal, ComponentModel.ISupportInitialize).BeginInit()
+        CType(dtvNuevoVector, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' dtvVectorPrincipal
+        ' 
+        dtvVectorPrincipal.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dtvVectorPrincipal.Location = New Point(39, 25)
+        dtvVectorPrincipal.Name = "dtvVectorPrincipal"
+        dtvVectorPrincipal.RowHeadersWidth = 51
+        dtvVectorPrincipal.Size = New Size(571, 56)
+        dtvVectorPrincipal.TabIndex = 0
+        ' 
+        ' dtvNuevoVector
+        ' 
+        dtvNuevoVector.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dtvNuevoVector.Location = New Point(27, 211)
+        dtvNuevoVector.Name = "dtvNuevoVector"
+        dtvNuevoVector.RowHeadersWidth = 51
+        dtvNuevoVector.Size = New Size(741, 53)
+        dtvNuevoVector.TabIndex = 1
+        ' 
+        ' cmdEjecutar
+        ' 
+        cmdEjecutar.Location = New Point(205, 342)
+        cmdEjecutar.Name = "cmdEjecutar"
+        cmdEjecutar.Size = New Size(206, 46)
+        cmdEjecutar.TabIndex = 2
+        cmdEjecutar.Text = "Ejecutar"
+        cmdEjecutar.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(cmdEjecutar)
+        Controls.Add(dtvNuevoVector)
+        Controls.Add(dtvVectorPrincipal)
+        Name = "Form1"
+        Text = "Form1"
+        CType(dtvVectorPrincipal, ComponentModel.ISupportInitialize).EndInit()
+        CType(dtvNuevoVector, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents dtvVectorPrincipal As DataGridView
+    Friend WithEvents dtvNuevoVector As DataGridView
+    Friend WithEvents cmdEjecutar As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dtvVectorPrincipal.ColumnCount = 10
+        dtvVectorPrincipal.Rows.Add(1)
+
+
+        For i = 0 To 9
+            dtvVectorPrincipal.Columns(i).Width = 40
+        Next
+
+
+        dtvNuevoVector.ColumnCount = 15
+        dtvNuevoVector.Rows.Add(1)
+        For i = 0 To 14
+            dtvNuevoVector.Columns(i).Width = 40
+
+        Next
+    End Sub
+
+    Private Sub cmdEjecutar_Click(sender As Object, e As EventArgs) Handles cmdEjecutar.Click
+        Dim VectorPrincipal(0 To 9) As Integer
+        Dim NuevoVector(0 To 14) As Integer
+		Dim contador As Integer
+		Dim i As Integer
+		Dim indice As Integer
+		Dim primerelemento As Integer
+		Dim segundoelemento As Integer
+		Dim tercerelemento As Integer
+
+
+		contador = 1
+		indice = 0
+		For i = 0 To 9
+			VectorPrincipal(i) = dtvVectorPrincipal.Item(i, 0).Value
+		Next i
+		For i = 0 To 9
+			If contador = 2 Then
+				NuevoVector(indice) = VectorPrincipal(i)
+				segundoelemento = VectorPrincipal(i)
+				indice = indice + 1
+				tercerelemento = primerelemento + segundoelemento
+				NuevoVector(indice) = tercerelemento
+				indice = indice + 1
+				contador = 1
+			Else
+				NuevoVector(indice) = VectorPrincipal(i)
+				primerelemento = VectorPrincipal(i)
+				indice = indice + 1
+				contador = contador + 1
+			End If
+		Next i
+
+		For i = 0 To 9
+			dtvVectorPrincipal.Item(i, 0).Value = VectorPrincipal(i)
+		Next i
+
+		For i = 0 To 14
+			dtvNuevoVector.Item(i, 0).Value = NuevoVector(i)
+		Next i
+
+	End Sub
+End Class
